b2b704efdef34d55a0e46b20fa1ecddb367625fa
ZihaoRe <reus@foxmail.com>
2020-01-26 19:16:11 +0800

    test

--- js/mockData.js
+++ js/mockData.js
@@ -1,39 +1,39 @@
 var data = [
     {
         img:'1.jpg',
-        title:'title1',
-        desc:'description1'
+        title:'猪',
+        desc:'女曰鸡鸣，士曰昧旦'
     }, {
         img:'2.jpg',
-        title:'title2',
-        desc:'description2'
+        title:'傻',
+        desc:'子兴视夜，明星有烂'
     }, {
         img:'3.jpg',
-        title:'title3',
-        desc:'description3'
+        title:'智障',
+        desc:'将翱将翔，弋凫与雁'
     }, {
         img:'4.jpg',
-        title:'title4',
-        desc:'description4'
+        title:'美',
+        desc:'弋言加之，与子宜之'
     },{
         img:'5.jpg',
-        title:'title5',
-        desc:'description5'
+        title:'仙女',
+        desc:'宜言饮酒，与子偕老'
     }, {
         img:'6.jpg',
-        title:'title6',
-        desc:'description6'
+        title:'美美美',
+        desc:'琴瑟在御，莫不静好'
     }, {
         img:'7.jpg',
-        title:'title7',
-        desc:'description7'
+        title:'磨叽王',
+        desc:'知子之来之，杂佩以赠之'
     }, {
         img:'8.jpg',
-        title:'title8',
-        desc:'description8'
+        title:'萌',
+        desc:'知子之顺之，杂佩以问之'
     }, {
         img:'9.jpg',
-        title:'title9',
-        desc:'description9'
+        title:'女神',
+        desc:'知子之好之，杂佩以报之'
     }
 ];
